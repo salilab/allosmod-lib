b842555c3b17f094b9faa8833aea806f5dc5f387
Ben Webb <ben@salilab.org>
2015-05-21 18:05:06 -0700

    Implement make_mod_inputs.

---
+++ lib/allosmod/make_mod_inputs.py
@@ -0,0 +1,70 @@
+"""Make input files for Modeller."""
+
+from __future__ import print_function, absolute_import
+import optparse
+import allosmod.util
+
+def make_mod_inputs(target, templates, rand_seed, rand_ang, deviation,
+                    nucleic_acids):
+    import modeller.automodel
+    import modeller.scripts
+    env = modeller.environ(rand_seed=rand_seed)
+    env.io.atom_files_directory = ['../atom_files']
+    env.libs.topology.read(file='$(LIB)/top_heav.lib')
+    env.libs.parameters.read(file='$(LIB)/par.lib')
+
+    # Read in HETATM records from template PDBs
+    env.io.hetatm = True 
+
+    mdl = modeller.scripts.complete_pdb(env, 'avgpdb.pdb')
+
+    # Act on all atoms in the model
+    sel = modeller.selection(mdl)
+
+    # Change all existing X,Y,Z for +- dev:
+    sel.rotate_mass_center([0,0,1], rand_ang[0])
+    sel.rotate_mass_center([0,1,0], rand_ang[1])
+    sel.rotate_mass_center([1,0,0], rand_ang[2])
+    sel.randomize_xyz(deviation=deviation)
+    mdl.write(file='random.ini')
+    a = modeller.automodel.automodel(env, deviation=None, alnfile='align.ali',
+                                     knowns=templates, sequence=target)
+    if nucleic_acids:
+        # use longer restraints for nucleic acids
+        a.max_sc_sc_distance = 14.0
+
+    a.make(exit_stage=1)
+
+def parse_args():
+    usage = """%prog [opts] <target> <template file>
+                            <rand seed> <dz> <dy> <dx> <deviation>
+
+Make input files for Modeller. Given an alignment file (align.ali) and
+a structure file (avgpdb.pdb), it will generate a randomized initial
+model (random.ini) and a restraints file.
+
+<target> is the target sequence.
+<template file> is a file listing the templates to use, one per line.
+<rand seed> is a number to use as the Modeller random number seed.
+<dz>, <dy>, <dx> are angles (in degrees) to rotate the initial model by
+                 about each axis (z, y, x).
+<deviation> is an amount (in angstroms) to randomize the model by.
+"""
+    parser = optparse.OptionParser(usage)
+    parser.add_option("--nucleic-acids", action="store_true", dest="nuc",
+                      help="use longer restraints for nucleic acids")
+
+    opts, args = parser.parse_args()
+    if len(args) != 7:
+        parser.error("incorrect number of arguments")
+    return (args[0], args[1], int(args[2]),
+            (float(args[3]), float(args[4]), float(args[5])),
+            float(args[6]), opts)
+
+def main():
+    target, template_file, rand_seed, rand_ang, deviation, opts = parse_args()
+    make_mod_inputs(target, allosmod.util.read_templates(template_file),
+                    rand_seed, rand_ang, deviation, opts.nuc)
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_make_mod_inputs.py
@@ -0,0 +1,58 @@
+import unittest
+import modeller
+import os
+import subprocess
+from test_pdb2ali import check_output
+
+class Tests(unittest.TestCase):
+    def test_bad(self):
+        """Test wrong arguments to make_mod_inputs"""
+        for args in ([], ['x'] * 8):
+            out = check_output(['allosmod', 'make_mod_inputs'] + args,
+                               stderr=subprocess.STDOUT, retcode=2)
+            out = check_output(['python', '-m',
+                                'allosmod.make_mod_inputs'] + args,
+                               stderr=subprocess.STDOUT, retcode=2)
+
+    def setup_inputs(self, seq='AW'):
+        with open('align.ali', 'w') as fh:
+            fh.write("""C; Sample alignment
+>P1;1fdx
+sequence:1fdx:1    : :54   : ::: 2.00:-1.00
+AY*
+>P1;5fd1
+structureX:5fd1:1    :A:2  :A::: 1.90: 0.19
+AV*
+""")
+        with open('templates', 'w') as fh:
+            fh.write("5fd1\n")
+        with open('5fd1', 'w') as fh:
+            fh.write("""
+ATOM      2  CA  ALA A   1      27.449  14.935   5.140  1.00 29.87           C
+ATOM      7  CA  VAL A   2      26.593  16.867   8.258  1.00120.51           C
+""")
+        with open('avgpdb.pdb', 'w') as fh:
+            fh.write("""
+ATOM      2  CA  ALA A   1      27.449  14.935   5.140  1.00 29.87           C
+ATOM      7  CA  TYR A   2      26.593  16.867   8.258  1.00120.51           C
+""")
+
+    def test_simple(self):
+        """Simple complete run of make_mod_inputs"""
+        self.setup_inputs()
+        check_output(['allosmod', 'make_mod_inputs', '--', '1fdx',
+                      'templates', '-3333', '3', '3', '3', '4'])
+        e = modeller.environ()
+        for fname in ('random.ini', '1fdx.ini'):
+            m = modeller.model(e, file=fname)
+            self.assertEqual([x.code for x in m.residues], ['A', 'Y'])
+            # Should have converted CA-only in all-atom model
+            self.assertEqual(len(m.atoms), 18)
+        with open('1fdx.rsr') as fh:
+            self.assertEqual(len(fh.readlines()), 78)
+        for f in ('templates', 'avgpdb.pdb', '5fd1', 'align.ali',
+                  'random.ini', '1fdx.ini', '1fdx.rsr'):
+            os.unlink(f)
+
+if __name__ == '__main__':
+    unittest.main()

db5d7ab3b4b973daf4efeb1da28ab49d60c3752b
Ben Webb <ben@salilab.org>
2015-06-09 17:12:30 -0700

    Implement contpres.

---
+++ lib/allosmod/contpres.py
@@ -0,0 +1,100 @@
+"""Get all residues involved in charge contacts."""
+
+from __future__ import print_function, absolute_import, division
+import optparse
+import modeller
+import math
+import allosmod.util
+
+charged_residues = dict.fromkeys(('ARG', 'HIS', 'LYS', 'HSD', 'HSE',
+                                  'HSP', 'HID', 'HIE', 'HIP', 'ASP', 'GLU'))
+
+def get_restrained_atoms(mdl, rsr_file):
+    for line in rsr_file:
+        spl = line.split()
+        if len(spl) > 10 and spl[5] == '2' and spl[4] != '1':
+            yield mdl.atoms[int(spl[8])-1], mdl.atoms[int(spl[9])-1]
+
+def charged_ca_pair(a1, a2):
+    return a1.name == 'CA' and a2.name == 'CA' \
+           and a1.residue.pdb_name in charged_residues \
+           and a2.residue.pdb_name in charged_residues
+
+class ChargedContactFinder(object):
+    def __init__(self, env, rsr_file, pdb_file):
+        self.env, self.rsr_file, self.pdb_file = env, rsr_file, pdb_file
+
+    def find(self):
+        m = self._m = modeller.model(self.env, file=self.pdb_file)
+        charge = [0] * len(m.residues)
+        total = [0] * len(m.residues)
+        for a1, a2 in get_restrained_atoms(m, open(self.rsr_file)):
+            r1 = a1.residue.index - 1
+            r2 = a2.residue.index - 1
+            total[r1] += 1
+            total[r2] += 1
+            if charged_ca_pair(a1, a2):
+                charge[r1] += 1
+                charge[r2] += 1
+        self._total = total
+        self._contacts = []
+        for n, z in enumerate(zip(charge, total)):
+            c, t = z
+            self._contacts.append((n+1, c if t > 143 else 0))
+
+    def print_contacts(self, fh):
+        for n, c in self._contacts:
+            print("%d %d" % (n, c), file=fh)
+
+    def print_all_buried(self, sclbreak, fh):
+        for n, c in enumerate(self._total):
+            if c > 143 and self._m.residues[n].pdb_name in charged_residues:
+                print("%d %s" % (n+1, sclbreak), file=fh)
+
+    def print_cdensity(self, cutoff, sclbreak, fh):
+        xavg = sum(x[1] for x in self._contacts) / len(self._contacts)
+        xsd = math.sqrt(sum((x[1]-xavg)**2 \
+                        for x in self._contacts) / len(self._contacts))
+        if xsd > 0.:
+            for n, c in self._contacts:
+                v = (c - xavg) / xsd
+                if abs(v) > cutoff:
+                    print("%d %s" % (n, sclbreak), file=fh)
+        elif cutoff < 0:
+            for n, c in self._contacts:
+                print("%d %s" % (n, sclbreak), file=fh)
+
+def parse_args():
+    usage = """%prog [opts] <restraint file> <PDB file> <sclbreak>
+
+Output all residues from <PDB file> that are involved in charge contacts
+(as specified by <restraint file>). Charged residues will also be output
+to break.dat (with the given <sclbreak> scale factor).
+"""
+    parser = allosmod.util.ModellerOptionParser(usage)
+    parser.add_option("--cdensity_cutoff", type=float, default=None,
+                      dest="cdensity_cutoff", metavar='FLOAT',
+                      help="If given, only residues that are buried and with "
+                           "a high charge density (> cutoff standard "
+                           "deviations above the mean) will be output; "
+                           "otherwise, all charged residues will be output.")
+
+    opts, args = parser.parse_args()
+    if len(args) != 3:
+        parser.error("incorrect number of arguments")
+    return args[0], args[1], args[2], opts
+
+def main():
+    rsr_file, pdb_file, sclbreak, opts = parse_args()
+    e = modeller.environ()
+    e.io.hetatm = True
+    a = ChargedContactFinder(e, rsr_file, pdb_file)
+    a.find()
+    a.print_contacts(open('contpres.dat', 'w'))
+    if opts.cdensity_cutoff is not None:
+        a.print_cdensity(opts.cdensity_cutoff, sclbreak, open('break.dat', 'a'))
+    else:
+        a.print_all_buried(sclbreak, open('break.dat', 'a'))
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_contpres.py
@@ -0,0 +1,55 @@
+import unittest
+import modeller
+import os
+import sys
+import subprocess
+import collections
+from test_pdb2ali import check_output
+
+test_dir = os.path.dirname(sys.argv[0])
+
+class Tests(unittest.TestCase):
+    def test_bad(self):
+        """Test wrong arguments to contpres"""
+        for args in ([], ['' * 4]):
+            out = check_output(['allosmod', 'contpres'] + args,
+                               stderr=subprocess.STDOUT, retcode=2)
+            out = check_output(['python', '-m',
+                                'allosmod.contpres'] + args,
+                               stderr=subprocess.STDOUT, retcode=2)
+
+    def test_simple(self):
+        """Simple complete run of contpres"""
+        with open('break.dat', 'w') as fh:
+            fh.write("foo\nbar\n")
+        out = check_output(['allosmod', 'contpres',
+                            os.path.join(test_dir, 'input',
+                                         'test_contpres.rsr'),
+                            os.path.join(test_dir, 'input',
+                                         'test_contpres.pdb'), '50.0'])
+        with open('break.dat') as fh:
+            content = fh.read()
+        self.assertEqual(content, "foo\nbar\n1 50.0\n3 50.0\n")
+        with open('contpres.dat') as fh:
+            content = fh.read()
+        self.assertEqual(content, "1 4\n2 0\n3 4\n")
+        os.unlink('break.dat')
+        os.unlink('contpres.dat')
+
+    def test_cdensity(self):
+        """Simple contpres in cdensity mode"""
+        with open('break.dat', 'w') as fh:
+            fh.write("foo\nbar\n")
+        out = check_output(['allosmod', 'contpres', '--cdensity_cutoff', '1.0',
+                            os.path.join(test_dir, 'input',
+                                         'test_contpres.rsr'),
+                            os.path.join(test_dir, 'input',
+                                         'test_contpres.pdb'), '50.0'])
+        with open('break.dat') as fh:
+            content = fh.read()
+        self.assertEqual(content, "foo\nbar\n2 50.0\n")
+        os.unlink('break.dat')
+        os.unlink('contpres.dat')
+
+if __name__ == '__main__':
+    unittest.main()

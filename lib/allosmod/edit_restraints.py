c58a7539386eae49f61fcbdbf98a904b912c8033
Ben Webb <ben@salilab.org>
2015-06-15 15:45:56 -0700

    Add atoms common to all nucleic acids.

--- lib/allosmod/edit_restraints.py
+++ lib/allosmod/edit_restraints.py
@@ -317,6 +317,8 @@ def get_beta(pdb_file):
     return beta
 
 def get_nuc_restrained(atm_name, res_name):
+    if atm_name in ('OP1', 'OP2', "O3'"):
+        return True
     if res_name in ('ADE', 'A', 'DA'):
         return atm_name in ('N1', 'C2', 'N3', 'C4', 'C5', 'C6', 'N6', 'N7',
                             'C8', 'N9')

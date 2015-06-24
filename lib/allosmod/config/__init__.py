339438e97f91958b1fc0cb1c2d85b3c6d97a1efa
Ben Webb <ben@salilab.org>
2015-06-24 14:49:45 -0700

    Add dummy config file so that tests work.

---
+++ lib/allosmod/config/__init__.py
@@ -0,0 +1,9 @@
+# Dummy config; should not be installed. This is here simply so that the
+# test suite works.
+
+import os
+
+datadir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       '..', '..', '..', 'data')
+local_scratch='/tmp'
+global_scratch='/scrapp'

350a7e0717e18e0b9fa6a49031d4c43ab613ee39
Ben Webb <ben@salilab.org>
2016-06-30 16:15:13 -0700

    Report bad glycosylation patches in allosmod.py.
    If the user provided an allosmod.py containing glycosylation
    patches, make sure that each residue refered to actually
    exists. Previously, this was not done, which could generate
    invalid restraints and thus a hard to diagnose failure when
    those restraints were converted to splines.

--- data/get_glyc_restraint.sh
+++ data/get_glyc_restraint.sh
@@ -4,6 +4,15 @@
 PM=$1 #pm.pdb file
 ALLOSPY=$2 #allosmod.py with patches, to be converted to harmonic restraints
 
+handle_missing_residue() {
+  local resnum="$1"
+  echo "Could not parse glycosylation patch in $ALLOSPY;" >&2
+  echo "residue $resnum not found in $PM." >&2
+  echo "Note that residue numbering in the model may not be the same" >&2
+  echo "as that in your input structure(s), particularly if those" >&2
+  echo "inputs have missing residues." >&2
+}
+
 R_CA=( `grep -A1 GLB ${ALLOSPY}| grep residues | sed "s/'//g" | awk 'BEGIN{FS="["}{print $2}' | awk 'BEGIN{FS=":"}(NR%2==1){print $1}'` \
        `grep -A1 GPA ${ALLOSPY}| grep residues | sed "s/'//g" | awk 'BEGIN{FS="["}{print $2}' | awk 'BEGIN{FS=":"}(NR%2==1){print $1}'` \ 
        `grep -A1 GPB ${ALLOSPY}| grep residues | sed "s/'//g" | awk 'BEGIN{FS="["}{print $2}' | awk 'BEGIN{FS=":"}(NR%2==1){print $1}'` )
@@ -21,6 +30,14 @@ for i in ${R_CA[@]}; do
 
     iatm1=`awk 'BEGIN{FS=""}($1$2$3$4=="ATOM"||$1$2$3$4=="HETA"){print $23$24$25$26$27,$14$15}' $PM | awk '($1=='${i}'&&$2=="CA"){print NR}'`
     iatm2=`awk 'BEGIN{FS=""}($1$2$3$4=="ATOM"||$1$2$3$4=="HETA"){print $23$24$25$26$27,$14$15}' $PM | awk '($1=='${j}'&&$2=="C1"){print NR}'`
+    if [ "$iatm1" = "" ]; then
+      handle_missing_residue $i
+      exit 1
+    fi
+    if [ "$iatm2" = "" ]; then
+      handle_missing_residue $j
+      exit 1
+    fi
 
     echo $iatm1 $iatm2 | awk '{printf "R    3   1   1   1   2   2   1    %5.0f %5.0f    5.0000    0.0350\n",$1,$2}'
 
--- test/test_get_glyc_restraint.py
+++ test/test_get_glyc_restraint.py
@@ -43,5 +43,27 @@ HETATM    6  C1  BMA B   2      19.244  16.654   7.119  1.00 15.42           O
         os.unlink('test.pdb')
         os.unlink('test-allosmod.py')
 
+    def test_bad_residue(self):
+        """Simple get_glyc_restraint with invalid residue"""
+        with open('test-allosmod.py', 'w') as fh:
+            fh.write("""line 1
+def special_patches(self, aln):
+    self.patch(residue_type='NGLB', residues=(self.residues['1:A'],
+                                              self.residues['2:B']))
+line 2
+""")
+        with open('test.pdb', 'w') as fh:
+            fh.write("""
+ATOM      1  N   ALA A   1      17.807  17.608   5.019  1.00 17.18           N
+ATOM      2  CA  ALA A   1      17.121  17.162   6.197  1.00 15.60           C
+ATOM      3  C   ALA A   1      18.085  17.018   7.343  1.00 14.54           C
+ATOM      4  O   ALA A   1      19.244  16.654   7.119  1.00 15.42           O
+""")
+        out = check_output(['allosmod', 'get_glyc_restraint', 'test.pdb',
+                            'test-allosmod.py'], stderr=subprocess.STDOUT,
+                           retcode=1)
+        os.unlink('test.pdb')
+        os.unlink('test-allosmod.py')
+
 if __name__ == '__main__':
     unittest.main()
